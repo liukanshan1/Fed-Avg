2c8dd8469ade307debc1d1bda4df84c0a2a5beea
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-10-14 14:20:12 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
 import scipy.io
 
 a = scipy.io.loadmat('./newData/JS00002.mat', 'val')
-
-
 print(a)
+
+import glob
+
+def get_all_mat(path):
+
+
+
+def get_all_files(path, file_type):
+    for file_abs in glob.glob(path) :
+    print(file_abs)
+
+
