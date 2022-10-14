4077472ceb2ee0c0b035d85be998e4ac10785608
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-10-14 21:52:48 +0800

    Update generate_results.py

--- generate_results.py
+++ generate_results.py
@@ -5,21 +5,6 @@ from sklearn.metrics import (confusion_matrix,
 import utils
 
 
-# %% Auxiliar functions
-def get_scores(y_true, y_pred, score_fun):
-    nclasses = np.shape(y_true)[1]
-    scores = []
-    for name, fun in score_fun.items():
-        scores += [[fun(y_true[:, k], y_pred[:, k]) for k in range(7)]]
-    return np.array(scores).T
-
-
-def specificity_score(y_true, y_pred):
-    m = confusion_matrix(y_true, y_pred, labels=[0, 1])
-    spc = m[0, 0] * 1.0 / (m[0, 0] + m[0, 1])
-    return spc
-
-
 def get_optimal_precision_recall(y_true, y_score):
     """Find precision and recall values that maximize f1 score."""
     n = np.shape(y_true)[1]
@@ -35,9 +20,10 @@ def get_optimal_precision_recall(y_true, y_score):
         index = np.argmax(f1_score)
         opt_precision.append(precision[index])
         opt_recall.append(recall[index])
-        t = threshold[index-1] if index != 0 else threshold[0]-1e-10
+        t = threshold[index - 1] if index != 0 else threshold[0] - 1e-10
         opt_threshold.append(t)
     return np.array(opt_precision), np.array(opt_recall), np.array(opt_threshold)
+
 
 def affer_results(y_true, y_pred):
     """Return true positives, false positives, true negatives, false negatives.
@@ -76,9 +62,10 @@ def affer_results(y_true, y_pred):
     cm[fp] = 3
     return tn, tp, fn, fp, cm
 
+
 # Get true values
 y_true = np.array(utils.get_all_hea("./data/test_set/"))
-y_pred = np.loadnp.load('./dnn_output.npy')
+y_pred = np.load('./dnn_output.npy')
 tn, tp, fn, fp, cm = affer_results(y_true, y_pred)
 print(cm.shape)
 print(cm)
